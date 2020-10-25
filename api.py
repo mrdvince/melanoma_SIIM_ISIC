9bf394ef1005eba91d3fa68cd6523152fcf9c17e
Droid021 <vincivenv@gmail.com>
2020-10-25 10:40:37 +0300

    feat: Model class inclusion

--- api.py
+++ api.py
@@ -3,6 +3,25 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static'
+MODEL = None
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class SEResNext50(nn.Module):
+    def __init__(self, pretrained='imagenet'):
+        super(SEResNext50, self).__init__()
+        self.models = pretrainedmodels.__dict__[
+            'se_resnext50_32x4d'](pretrained=pretrained)
+        self.l0 = nn.Linear(2048, 1)
+
+    def forward(self, image, targets):
+        bs, _, _, _ = image.shape
+        x = self.models.features(image)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.reshape(bs, -1)
+        out = self.l0(x)
+        loss = nn.BCEWithLogitsLoss()(out, targets.reshape(-1, 1).type_as(out))
+        return out, loss
 
 
 @app.route('/', methods=['GET', 'POST'])
