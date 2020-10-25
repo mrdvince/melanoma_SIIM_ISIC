1320071e523159aefa34420f96a36ca84126ab93
Droid021 <vincivenv@gmail.com>
2020-10-25 10:23:23 +0300

    Render html ui

---
+++ api.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
