bad81bdda71443bdf32682c88a7c256644479166
Droid021 <vincivenv@gmail.com>
2020-10-25 10:37:22 +0300

    Upload Images

--- api.py
+++ api.py
@@ -1,11 +1,22 @@
+import os
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
-    return render_template('index.html')
+    if request.method == 'POST':
+        image_file = request.files['image']
+        if image_file:
+            image_file.save(os.path.join(
+                UPLOAD_FOLDER,
+                image_file.filename
+            ))
+            return render_template('index.html', prediction=1)
+
+    return render_template('index.html', prediction=0)
 
 
 if __name__ == '__main__':
